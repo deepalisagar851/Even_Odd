3921b54e087eb9d5ab2a01875ac3caf376656010
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 14:45:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Even_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("The Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Odd");
+            }
+            }
+        }
+    }
+
